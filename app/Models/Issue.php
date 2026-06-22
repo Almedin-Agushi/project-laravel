8a9d7e49c680b3465929ef94ca50e66d8b683884
Almedin Agushi <77640112+Almedin-Agushi@users.noreply.github.com>
2026-06-22 19:57:16 +0200

    Initial Laravel setup

---
+++ app/Models/Issue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Issue extends Model
+{
+    public function project()
+{
+    return $this->belongsTo(Project::class);
+}
+
+public function comments()
+{
+    return $this->hasMany(Comment::class);
+}
+
+public function tags()
+{
+    return $this->belongsToMany(Tag::class);
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\TagController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::resource('projects', ProjectController::class);
+Route::resource('issues', IssueController::class);
+Route::resource('tags', TagController::class);
